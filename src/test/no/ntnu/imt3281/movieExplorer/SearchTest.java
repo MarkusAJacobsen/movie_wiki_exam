86871f991d994d247f1aa2d955c0ece9ab0d219d
Øivind Kolloen <oeivind.kolloen@ntnu.no>
2017-10-18 14:53:45 +0200

    Added tests for Genres, updated the test for Search

--- src/test/no/ntnu/imt3281/movieExplorer/SearchTest.java
+++ src/test/no/ntnu/imt3281/movieExplorer/SearchTest.java
@@ -9,11 +9,11 @@ public class SearchTest {
 
 	@Test
 	public void test() {
+		// Multisearch should use the search feature described on https://developers.themoviedb.org/3/search/multi-search
+		// Return the result as a JSON object.
 		JSON result = Search.multiSearch("Bruce Willis");
-		assertEquals(2, result.get("results").size());
-		assertEquals("Bruce Willis", result.get("results").get(0).getValue("name"));
-		assertEquals("person", result.get("results").get(0).getValue("media_type"));
-		
+		assertEquals(2, result.get("results").size());									// A search for Bruce Willis return two results
+		assertEquals("Bruce Willis", result.get("results").get(0).getValue("name"));		// The first is to Bruce Willis
+		assertEquals("person", result.get("results").get(0).getValue("media_type"));		// the person
 	}
-
 }
