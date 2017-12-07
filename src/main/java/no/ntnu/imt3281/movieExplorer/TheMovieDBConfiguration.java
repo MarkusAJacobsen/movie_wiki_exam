718765aa25a18a3fc8ea7281e6f7220a2da94be9
MarkusJacobsen <markusja@stud.ntnu.no>
2017-12-07 17:57:41 +0100

    Task 4 comments

--- src/main/java/no/ntnu/imt3281/movieExplorer/TheMovieDBConfiguration.java
+++ src/main/java/no/ntnu/imt3281/movieExplorer/TheMovieDBConfiguration.java
@@ -1,17 +1,28 @@
 package no.ntnu.imt3281.movieExplorer;
 
 /**
- * 
+ * This class setsup all configuration for images
+ * by returning correct url and size for wanted image and type
  */
 public class TheMovieDBConfiguration {
     public JSON configuration;
     private String base_url = null;
 
+    /**
+     * Constructor takes a JSON string and creates a JSON to
+     * represent the available configurations
+     * @param json configuration string from themoviedb
+     */
     public TheMovieDBConfiguration(String json) {
         configuration = new JSON(json);
         base_url = (String) configuration.get(0).get(3).getValue("base_url");
     }
 
+    /**
+     * Returns correct URL for BackDrop Image
+     * @param query image to include in URL
+     * @return URL for BackDropImage
+     */
     public String getBackdropURL(String query) {
         String result;
 
@@ -23,6 +34,11 @@ public class TheMovieDBConfiguration {
         return result;
     }
 
+    /**
+     * Returns correct URL for Logo Image
+     * @param query image to include in URL
+     * @return URL for LogoImage
+     */
     public String getLogoURL(String query) {
         String result;
 
@@ -34,6 +50,11 @@ public class TheMovieDBConfiguration {
         return result;
     }
 
+    /**
+     * Returns correct URL for Poster image
+     * @param query image to include in URL
+     * @return URL for Poster Image
+     */
     public String getPosterURL(String query) {
         String result;
 
@@ -45,6 +66,11 @@ public class TheMovieDBConfiguration {
         return result;
     }
 
+    /**
+     * Returns correct URL for Profile Image
+     * @param query image to include in URL
+     * @return URL for Profile Image
+     */
     public String getProfileURL(String query) {
         String result;
 
@@ -56,6 +82,11 @@ public class TheMovieDBConfiguration {
         return result;
     }
 
+    /**
+     * Returns correct URL for Still Image
+     * @param query image to include in URL
+     * @return URL for Still Image
+     */
     public String getStillURL(String query) {
         String result;
 
