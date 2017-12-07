39a60345add3bf5088329951eaecc7af023b0dab
MarkusJacobsen <markusja@stud.ntnu.no>
2017-12-07 16:14:31 +0100

    Task 3 finished with DB

--- src/main/java/no/ntnu/imt3281/movieExplorer/Genres.java
+++ src/main/java/no/ntnu/imt3281/movieExplorer/Genres.java
@@ -3,28 +3,55 @@ package no.ntnu.imt3281.movieExplorer;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import java.util.Objects;
+
+/**
+ * This class checks if a genre is registered in the DB
+ * If not, go to themoviedb and fetch an updated list
+ */
 public class Genres {
 
-
-
+    /**
+     * Checks if a genre is in DB if not fetch from themovieDB
+     * @param i External ID to fetch
+     * @return corresponding name of genre to the i
+     */
     public static String resolve(int i) {
         GenresDB DB = GenresDB.getInstance();
 
-        if(!DB.GenreInDb(i)) {
-            String req;
-            JSON obj;
+        if(!DB.GenreInDb(Integer.toString(i))) {
+            String reqMovie, reqTv;
+            JSON objMovie, objTv;
             try {
-                req = Unirest.get("https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=a47f70eb03a70790f5dd711f4caea42d").asString().getBody();
-                obj = new JSON(req);
+                reqMovie = Unirest.get("https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=a47f70eb03a70790f5dd711f4caea42d").asString().getBody();
+                reqTv = Unirest.get("https://api.themoviedb.org/3/genre/tv/list?api_key=a47f70eb03a70790f5dd711f4caea42d&language=en-US").asString().getBody();
+                objMovie = new JSON(reqMovie);
+                objTv = new JSON(reqTv);
+                JSON genresMovie = objMovie.get("genres");
+                JSON genresTV = objTv.get("genres");
 
-                for(int j = 0; j < obj.size(); j++) {
-                    DB.addGenres(Integer.parseInt(obj.key), obj.value.toString());
+                for(int j = 0; j < genresMovie.size(); j++) {
+                    JSON o = genresMovie.get(j);
+                    DB.addGenres(o.getValue("id").toString(), o.getValue("name").toString());
+                }
+
+                for(int j = 0; j < genresTV.size(); j++) {
+                    GenresDB DB2 = GenresDB.getInstance();
+                    JSON o = genresTV.get(j);
+                    DB2.addGenres(o.getValue("id").toString(), o.getValue("name").toString());
+                }
+
+                GenresDB DB3 = GenresDB.getInstance();
+                String result = DB3.fetchField(i);
+                if(!Objects.equals(result, "")){
+                    return "-1";
                 }
             } catch (UnirestException e) {
                 e.printStackTrace();
             }
 
-        }
+        } else
+            return DB.fetchField(i);
         return "";
     }
 }
--- src/main/java/no/ntnu/imt3281/movieExplorer/GenresDB.java
+++ src/main/java/no/ntnu/imt3281/movieExplorer/GenresDB.java
@@ -2,12 +2,23 @@ package no.ntnu.imt3281.movieExplorer;
 
 import java.sql.*;
 
+/**
+ * GenresDB is the database controller for the Genres DB.
+ * This db contains registered genres from themoviedb
+ *
+ * The code is mostly refactored from the lectures and project 2 in this subject
+ */
 public class GenresDB {
     private Connection con;
     private final String url = "jdbc:derby:genres.db";
     private boolean connectedToDB = false;
     private static GenresDB instance = null;
+    private static final String TABLENAME = "Genres";
 
+    /**
+     * Constructor, setsup connection and creates the table
+     * @throws SQLException
+     */
     private GenresDB() throws SQLException
     {
         try
@@ -24,6 +35,11 @@ public class GenresDB {
 
     }
 
+    /**
+     * This DB is sat up as a singleton, so if it has been instantiated
+     * before, only return the "living" instance
+     * @return Instance of this DB
+     */
     public static GenresDB getInstance() {
         if(instance == null) {
             try {
@@ -35,13 +51,17 @@ public class GenresDB {
         return instance;
     }
 
+    /**
+     * Creates a table with to fields, number and name representing the field from
+     * themoviedb. Also has an internal incrementor
+     */
     private void createTable() {
         Statement stm = null;
 
         try                                         // Check if table exist already
         {
             stm = con.createStatement();
-            ResultSet tableExists = stm.executeQuery("SELECT * FROM Genres");
+            ResultSet tableExists = stm.executeQuery("SELECT * FROM "+ TABLENAME);
             tableExists.close();
             stm.close();
         }
@@ -50,11 +70,10 @@ public class GenresDB {
             try                                     // Create the table
             {
                 stm = con.createStatement();
-                String createTable = "CREATE TABLE IF NOT EXISTS Genres " +
-                        "(Id INTEGER(11) NOT NULL, " +
-                        " Name VARCHAR(255), " +
-                        " PRIMARY KEY ( Id ))";
-                stm.executeUpdate(createTable);
+                stm.execute("CREATE TABLE " + TABLENAME +"(id bigint NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
+                       + "number VARCHAR(255) NOT NULL, "
+                       + " name varchar(255), "
+                       + " PRIMARY KEY ( id ))");
                 stm.close();
             }
             catch (SQLException e2)
@@ -64,7 +83,13 @@ public class GenresDB {
         }
     }
 
-    public int addGenres(int id, String name) {
+    /**
+     * Adds a genre to the DB
+     * @param id External ID
+     * @param name External genre name
+     * @return return code
+     */
+    public int addGenres(String id, String name) {
         if (!connectedToDB)                          // Is there connection with DB?
             throw new IllegalStateException("Not contact with DB");
         // check if username is used
@@ -76,9 +101,9 @@ public class GenresDB {
         {
             PreparedStatement stm = null;
             try {
-                stm = con.prepareStatement("INSERT INTO Genres (Id, Name) VALUES (?, ?)");
+                stm = con.prepareStatement("INSERT INTO "+ TABLENAME+" (Number, Name) VALUES (?, ?)");
 
-                stm.setInt(1, id); // Make the statement ready
+                stm.setString(1, id); // Make the statement ready
                 stm.setString(2, name); // hashed password
 
                 // Execute to update
@@ -94,16 +119,21 @@ public class GenresDB {
         return 0;
     }
 
-    public boolean GenreInDb(Integer id) {
+    /**
+     * Checks if an external ID is registered in the DB before
+     * @param id External ID to check if exist
+     * @return true/false found/not Found
+     */
+    public boolean GenreInDb(String id) {
         ResultSet res;                              // Variable to store the result
-        String statement = "SELECT Id FROM Genres"
-                + "\n WHERE Id=?";
+        String statement = "SELECT Number FROM " +TABLENAME
+                + "\n WHERE Number=?";
 
         PreparedStatement stm = null;
         try {
             stm = con.prepareStatement(statement);
             // Prepare and execute the statment
-            stm.setInt(1, id);
+            stm.setString(1, id);
             res = stm.executeQuery();
 
             if (!res.next())
@@ -123,4 +153,38 @@ public class GenresDB {
         return false;
     }
 
+    /**
+     * Fetch the "name" field of a DB post
+     * @param i External ID to fetch
+     * @return corresponding name for supplied i
+     */
+    public String fetchField(int i) {
+        ResultSet res;
+        String statement = "SELECT Number, Name FROM " +TABLENAME
+                + "\n WHERE Number=?";
+
+        PreparedStatement stm = null;
+        try {
+            stm = con.prepareStatement(statement);
+            // Prepare and execute the statment
+            stm.setString(1, Integer.toString(i));
+            res = stm.executeQuery();
+
+            if (!res.next())
+            {
+                stm.close();
+                return "";
+            }
+            // User found
+            else
+            {
+                String result = res.getString("Name");
+                stm.close();
+                return result;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
