a86e94f847db6db6c293675eda2c579a691a396f
MarkusJacobsen <markusja@stud.ntnu.no>
2017-12-07 12:52:56 +0100

    WIP task 1 pt 3

--- src/main/java/no/ntnu/imt3281/movieExplorer/JSON.java
+++ src/main/java/no/ntnu/imt3281/movieExplorer/JSON.java
@@ -3,20 +3,33 @@ package no.ntnu.imt3281.movieExplorer;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.Iterator;
 
 
 public class JSON {
     public JSONObject obj;
 
     public JSON(String jsonInput) {
+        this.obj = new JSONObject();
         JSONParser parser = new JSONParser();
         try {
             this.obj = (JSONObject) parser.parse(jsonInput);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+    }
+
+    public JSON(Object tmp) {
+        this.obj = new JSONObject();
+        try {
+            this.obj.put("tmp", tmp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JSON (JSONObject tmp) {
+        this.obj = tmp;
     }
 
     public <T> T getValue(String query) {
@@ -26,29 +39,31 @@ public class JSON {
                 return (T) value.toString();
             } else if (value instanceof Long) {
                 return value;
-            }
+            } else
+                return value;
 
         }
         return (T) "";
     }
 
     public JSON get(String query) {
-        Object tmp = obj.get(query);
-
+        Object tmp =  obj.get(query);
         if(tmp != null) {
             if(obj.get(query) instanceof JSONArray) {
-                return new JSON(tmp.toString());
+                return new JSON(tmp);
             }
         }
         return null;
     }
 
     public int size() {
-        return 0;
+        return this.obj.size();
     }
 
 
     public JSON get(int i) {
-        return new JSON("he");
+        JSONArray tmp = (JSONArray) this.obj.get("tmp");
+        JSONObject tmp2 = (JSONObject) tmp.get(i);
+        return new JSON(tmp2);
     }
 }
